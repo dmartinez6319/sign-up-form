0466ff2d7acc9aaa05f0b97f57fe5e5b4cd1c1bd
Daniel Martinez <dmartinez6319@gmail.com>
2024-07-10 02:13:05 -0400

    Password Confirm logic added

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+const PASSINPUT = document.querySelector("#password");
+const CONFIRMPASSINPUT = document.querySelector("#confirm-pass");
+const CREATEBUTTON = document.querySelector("#create-button");
+const WARNINGTEXT = document.querySelector("#notify");
+
+
+PASSINPUT.addEventListener("input", () => {
+    if (PASSINPUT.value !== CONFIRMPASSINPUT.value) {
+        document.documentElement.style.setProperty("--ghost",1);
+        CREATEBUTTON.setAttribute("disabled",true);
+    } else {
+        document.documentElement.style.setProperty("--ghost",0);
+        CREATEBUTTON.removeAttribute("disabled",false);
+    }
+})
+
+CONFIRMPASSINPUT.addEventListener("input", () => {
+    if (PASSINPUT.value !== CONFIRMPASSINPUT.value) {
+        document.documentElement.style.setProperty("--ghost",1);
+        PASSINPUT.setCustomValidity("Invalid field.");
+        CONFIRMPASSINPUT.setCustomValidity("Invalid field.");
+        CREATEBUTTON.setAttribute("disabled",true);
+    } else {
+        document.documentElement.style.setProperty("--ghost",0);
+        PASSINPUT.setCustomValidity("");
+        CONFIRMPASSINPUT.setCustomValidity("");
+        CREATEBUTTON.removeAttribute("disabled",false);
+    }
+})
+
+
 
